16ec3bd427dd385820fd35c91eeba0d601ada070
munkhorgil <munkhorgil@live.com>
2021-04-18 23:17:27 +0800

    add some packages

--- apis/api-data/src/index.ts
+++ apis/api-data/src/index.ts
@@ -3,9 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send(`Hello World`);
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
+  // tslint:disable-next-line: no-console
   console.log("Server listening on port 3000!");
 });
