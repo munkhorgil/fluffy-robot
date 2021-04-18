638fb812e56204358c91a36e11f0dc4d999f116b
munkhorgil <munkhorgil@live.com>
2021-04-19 00:17:14 +0800

    little update

--- apis/api-data/src/index.ts
+++ apis/api-data/src/index.ts
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv';
 import express from "express";
+
+dotenv.config();
+
+const { PORT } = process.env;
 
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send("Hello World");
+  res.send("There is no place like 127.0.0.1");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   // tslint:disable-next-line: no-console
-  console.log("Server listening on port 3000!");
+  console.log("Server listening on port 3500");
 });
